7d23e6dcbe4deb4e8af73160d977a2085ba3bec5
Ansari Umer <umer37454@gmail.com>
2023-02-25 22:26:15 +0530

    Project added

---
+++ flexbox mini project/script.js
@@ -0,0 +1,7 @@
+const icon = document.querySelector(".fas");
+const sidebar = document.querySelector(".side");
+
+icon.addEventListener("click", () => {
+  sidebar.classList.toggle("sidemenu");
+
+});
