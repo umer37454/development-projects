4b52a504cc915da32557c3882bc0e86536b51c18
Ansari Umer <umer37454@gmail.com>
2023-03-15 16:16:33 +0530

    async await

--- promises/script.js
+++ promises/script.js
@@ -11,7 +11,7 @@ const delayedColorChange = (color, delay) => {
 
 }
 
-/*Executing a promise*/
+/*Executing a promise
 
 delayedColorChange('red', 1000)
   .then(() => delayedColorChange('orange', 1000))
@@ -19,3 +19,34 @@ delayedColorChange('red', 1000)
   .then(() => delayedColorChange('green', 1000))
   .then(() => delayedColorChange('yellow', 1000))
   .then(() => delayedColorChange('grey', 1000))
+
+/* Call back hell in promise also so make then return promise object
+
+delayedColorChange('red', 1000)
+  .then(() => {
+    delayedColorChange('orange', 1000)
+    .then(() => {
+      delayedColorChange('teal', 1000)
+      .then(() => {
+        delayedColorChange('green', 1000)
+      })
+    })
+  })
+
+  */
+
+/* Async Await
+ async return promise
+ await returns resolved promise
+ and if data is return with await we can capture it in var
+ and if something is returned with async then that will act as data */
+
+const rainbow = async () => {
+  await delayedColorChange('red', 1000)
+  await delayedColorChange('orange', 1000)
+  await delayedColorChange('green', 1000)
+  await delayedColorChange('teal', 1000)
+  await delayedColorChange('black', 1000)
+  delayedColorChange('white', 1000)
+}
+
